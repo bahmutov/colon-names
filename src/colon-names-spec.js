2033a7ac30f76a386bf500ec2630694a958824bf
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-11-02 09:27:59 -0400

    initial

---
+++ src/colon-names-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const colonNames = require('.')
+
+describe('colon-names', () => {
+  it('write this test', () => {
+    console.assert(colonNames, 'should export something')
+  })
+})
